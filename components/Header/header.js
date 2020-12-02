1e91645bca886196ec7957f94f390d3f858f9cd4
KhasanAlimov <hasanalimov021@gmail.com>
2020-12-02 15:12:47 +0600

    logo-title

--- components/Header/header.js
+++ components/Header/header.js
@@ -9,7 +9,7 @@ class Header {
 		<div class="header-container">
 			<div class="header-logo">
 				<h3>carSShop</h3>
-				<p class="logo-title">demo landing page</p>
+				<p class="logo-title">Demo landing page</p>
 			</div>
 			<div class="quantity-block" onclick="headerPage.openShoping()">
 				<img src="img/Quantity-icon2.png" alt="Quantity" class="quantity-icon">
