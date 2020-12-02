106b60eb8a3a2a8a0eeb7b8f82d25cc1541dd877
KhasanAlimov <hasanalimov021@gmail.com>
2020-12-02 16:26:27 +0600

    footer

--- constants/root.js
+++ constants/root.js
@@ -3,3 +3,4 @@ const ROOT_PRODUCTS = document.getElementById('products');
 const ROOT_SHOPING = document.getElementById('shoping');
 const ROOT_SPINNER = document.getElementById('spinner');
 const ROOT_ERROR = document.getElementById('error');
+const ROOT_FOOTER = document.getElementById('footer');
