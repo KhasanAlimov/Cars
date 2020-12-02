8ea69e821020ad09a82c45b691c0c31991b7fa95
KhasanAlimov <hasanalimov021@gmail.com>
2020-12-02 16:26:04 +0600

    footer

---
+++ components/Footer/footer.js
@@ -0,0 +1,7 @@
+const html = `
+  <div class="footer-container">
+    <div class="img-box">
+      <img src="components/Footer/img/car.png" alt="Car" class="footer-img">
+    </div>
+	</div>`;
+ROOT_FOOTER.innerHTML = html;
